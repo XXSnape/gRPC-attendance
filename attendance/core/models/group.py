c80283cac767d9776090b3540ea46a9f57712fa3
Snape <email>
2025-09-24 21:30:20 +0300

    Write group model

--- attendance/core/models/group.py
+++ attendance/core/models/group.py
@@ -1,10 +1,28 @@
+import uuid
+
+from sqlalchemy import UniqueConstraint, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
-from .enums.groups import GroupEnum
+from .enums.group import GroupEnum
 from .mixins.id_uuid import UUIDIdMixin
 
 
 class Group(UUIDIdMixin, Base):
+    __table_args__ = (
+        UniqueConstraint(
+            "name",
+            "type",
+            "year_of_admission",
+            name="idx_uniq_group",
+        ),
+    )
     name: Mapped[str] = mapped_column(unique=True)
     type: Mapped[GroupEnum]
+    year_of_admission: Mapped[int]
+    department_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey(
+            "departments.id",
+            ondelete="CASCADE",
+        )
+    )
