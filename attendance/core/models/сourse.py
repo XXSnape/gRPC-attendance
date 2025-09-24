9a1ac15fa5c5629ef77930b22c6d84dc644ed0f6
Snape <email>
2025-09-24 21:26:01 +0300

    Delete course model

--- attendance/core/models/сourse.py
+++
@@ -1,9 +0,0 @@
-from .mixins.id_uuid import UUIDIdMixin
-from sqlalchemy.orm import Mapped
-
-from .base import Base
-from .mixins.id_uuid import UUIDIdMixin
-
-
-class Course(Base):
-    year_of_admission: Mapped[int]
