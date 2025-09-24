a4a7ba2f1caec22338b7c19cb2dc1a8d2b8475e6
Snape <email>
2025-09-24 21:58:04 +0300

    Write user group model

--- attendance/core/models/user_group.py
+++ attendance/core/models/user_group.py
@@ -1,1 +1,32 @@
-# class UserGroup(Base)
+import uuid
+
+from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+from .mixins.id_uuid import UUIDIdMixin
+
+
+class UserGroup(UUIDIdMixin, Base):
+    __tablename__ = "users_groups"
+
+    __table_args__ = (
+        UniqueConstraint(
+            "user_id",
+            "group_id",
+            name="idx_uniq_user_group",
+        ),
+    )
+
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey(
+            "users.id",
+            ondelete="CASCADE",
+        )
+    )
+    group_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey(
+            "groups.id",
+            ondelete="CASCADE",
+        )
+    )
