00c88a1e79622c1031845dd2ae01bf89a263ac43
Snape <email>
2025-09-24 21:29:33 +0300

    Write lesson model

--- attendance/core/models/lesson.py
+++ attendance/core/models/lesson.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -7,6 +7,13 @@ import uuid
 
 
 class Lesson(UUIDIdMixin, Base):
+    __table_args__ = (
+        UniqueConstraint(
+            "name",
+            "department_id",
+            name="idx_uniq_name_department_id",
+        ),
+    )
     name: Mapped[str] = mapped_column(unique=True)
     department_id: Mapped[uuid.UUID] = mapped_column(
         ForeignKey(
