050441dd893661aa4287d99f342971102284ad00
Snape <email>
2025-10-11 14:52:23 +0300

    Move the database files

--- attendance/core/databases/sql/models/__init__.py
+++ attendance/core/databases/sql/models/__init__.py
@@ -7,16 +7,22 @@ from .department import Department as Department
 from .group import Group as Group
 from .lesson import Lesson as Lesson, LessonType as LessonType
 
-
 from .schedule import (
     Schedule as Schedule,
     GroupSchedule as GroupSchedule,
     PersonalSchedule as PersonalSchedule,
 )
-
+from .schedule_exceptions import ScheduleException
 
 from .teacher_department import TeacherDepartment as TeacherDepartment
-from .user import User as User, Student as Student
+from .user import (
+    User as User,
+    Student as Student,
+    Teacher as Teacher,
+    Administrator as Administrator,
+)
 
 from .user_group import UserGroup as UserGroup
 from .specialization import Specialization as Specialization
+from .teacher_schedule import TeacherSchedule as TeacherSchedule
+from .user_group import UserGroup as UserGroup
